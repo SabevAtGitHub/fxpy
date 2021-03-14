9a550b0ccfd98e58f18ac3cb35e4d2021b7c4c95
tt <tt3d@start.bg>
2021-03-14 11:27:51 +0100

    tests

--- tests/test_fx.py
+++ tests/test_fx.py
@@ -1,10 +1,21 @@
-# test_fx.py
+import pytest
+
+import os
+import sys 
+
+import numpy as np
+import pandas as pd
 
 import fx
 
+def test_me():
+    assert True
 
 def test_update_on():
-    
+    first = pd.DataFrame()
+    second = pd.DataFrame()
 
+    result = fx.update_on(first, second, on='A')
 
-    fx.update_on(first, second, on='B')
+    assert type(result) == type(second)
+    # assert type re
